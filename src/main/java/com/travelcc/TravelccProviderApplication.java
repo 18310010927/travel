51868b08e0053dd44dfdde1384881c663e6efc7d
bhx <18310010927@163.com>
2018-07-24 09:56:56 +0800

    IDEA上传GitHub

--- src/main/java/com/travelcc/TravelccProviderApplication.java
+++ src/main/java/com/travelcc/TravelccProviderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravelccProviderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TravelccProviderApplication.class, args);
+        SpringApplication.run(TravelccProviderApplication.class, args);
 
 	}
 }
